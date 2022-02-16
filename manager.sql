bd1196c76f1b8d005f8da1f08f7f8dc70b188199
felipetarran <dev.felipetarran@gmail.com>
2022-02-16 15:35:10 -0300

    Adding database dump

---
+++ manager.sql
@@ -0,0 +1,35 @@
+-- Adminer 4.8.1 MySQL 8.0.28-0ubuntu0.20.04.3 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE `tasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tarefa` varchar(255) NOT NULL,
+  `descricao` text NOT NULL,
+  `minutos` int NOT NULL,
+  `projeto` varchar(70) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `tasks` (`id`, `tarefa`, `descricao`, `minutos`, `projeto`) VALUES
+(26,	'daq',	'dsa',	1,	'Fa colchoes');
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) COLLATE utf8mb4_0900_as_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_0900_as_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_ci;
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1,	'lipe',	'admin'),
+(2,	'Jimi Hendrix',	'123');
+
+-- 2022-02-16 18:33:42
